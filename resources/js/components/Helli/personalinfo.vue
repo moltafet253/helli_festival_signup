f5bbb028dc68c3608b43898c465971ad0e2e44cb
Mohammad Ashouri <105170552+moltafet253@users.noreply.github.com>
2023-04-18 18:59:38 +0430

    Personal Image Upload (part 2) Completed

--- resources/js/components/Helli/personalinfo.vue
+++ resources/js/components/Helli/personalinfo.vue
@@ -3,7 +3,7 @@
         <h1></h1>
         <form>
             <div class="flex items-center ">
-                <span class="text-orange-500 pl-4">◼️</span>
+                <span class="text-orange-500 pl-4">◼</span>
                 <h3 class="text-lg font-bold ">اطلاعات‌ شخصی</h3>
                 <hr class="w-full border-t-2 border-b-orange mr-4">
             </div>
@@ -11,22 +11,24 @@
                 <div class="w-full lg:w-4/12 px-4">
                     <div class="relative flex items-center justify-center mx-auto mb-2 ">
 
-                        <img class="rounded-3xl w-32 h-32 object-cover" src="" alt="عکس کاربر">
+                        <img v-if="imageSrc" :src="imageSrc" class="rounded-3xl w-32 h-32 object-cover" src="{{ }}" alt="عکس کاربر">
 
-                        <div class="form-control w-full max-w-xs">
+                        <div v-else class="form-control w-full max-w-xs">
                             <form @submit.prevent="submitForm">
 
                                 <label class="label">
                                     <span class="label-text">لطفا تصویر پرسنلی خود را از کادر زیر انتخاب کرده و بر روی دکمه بارگذاری کلیک نمایید</span>
                                 </label>
                                 <br><br>
-                                <input type="file" name="file" ref="fileInput"
+                                <input type="file" name="file" ref="fileInput" accept=".jpg,.jpeg,.bmp,.png"
                                        class="file-input file-input-bordered w-full max-w-xs"/>
 
                                 <label class="label">
                                     <span class="label-text-alt">حداکثر حجم فایل: 2 مگابایت</span>
                                     <br>
                                     <span class="label-text-alt">با پسوندهای: png-jpg-jpeg</span>
+                                    <br>
+                                    <span class="label-text-alt">حداقل عرض و ارتفاع عکس: 128px*128px</span>
                                 </label>
                                 <br>
                                 <button type="submit"
@@ -42,14 +44,14 @@
                 </div>
                 <div class="w-full lg:w-4/12 px-4 flex-row">
                     <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">نام</label>
+                        <label class="block uppercase  text-xs font-bold mb-2">نام</label>
                         <label
                             class="cursor-not-allowed border border-colorborder px-3 py-3 bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
                                 datapersonal.FirstName
                             }}</label>
                     </div>
                     <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">نام پدر</label>
+                        <label class="block uppercase  text-xs font-bold mb-2">نام پدر</label>
                         <label
                             class="cursor-not-allowed border border-colorborder px-3 py-3 bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
                                 datapersonal.FatherName
@@ -58,7 +60,7 @@
                 </div>
                 <div class="w-full lg:w-4/12 px-4 flex-row">
                     <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">نام
+                        <label class="block uppercase  text-xs font-bold mb-2">نام
                             خانوادگی</label>
                         <label
                             class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
@@ -66,25 +68,25 @@
                             }}</label>
                     </div>
                     <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">کد ملی</label>
+                        <label class="block uppercase  text-xs font-bold mb-2">کد ملی</label>
                         <label
                             class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
                                 datapersonal.SocialID
                             }}</label>
                     </div>
                 </div>
+<!--                <div class="w-full lg:w-4/12 px-4 flex-row">-->
+<!--                    <div class="relative w-full mb-3">-->
+<!--                        <label class="block uppercase  text-xs font-bold mb-2">شماره-->
+<!--                            شناسنامه</label>-->
+<!--                        <label-->
+<!--                            class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{-->
+<!--                            }}</label>-->
+<!--                    </div>-->
+<!--                </div>-->
                 <div class="w-full lg:w-4/12 px-4 flex-row">
                     <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">شماره
-                            شناسنامه</label>
-                        <label
-                            class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
-                            }}</label>
-                    </div>
-                </div>
-                <div class="w-full lg:w-4/12 px-4 flex-row">
-                    <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">تاریخ
+                        <label class="block uppercase  text-xs font-bold mb-2">تاریخ
                             تولد</label>
                         <label
                             class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
@@ -94,30 +96,30 @@
                 </div>
                 <div class="w-full lg:w-4/12 px-4 flex-row">
                     <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">جنسیت</label>
+                        <label class="block uppercase  text-xs font-bold mb-2">جنسیت</label>
                         <label
                             class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
                                 datapersonal.Gender
                             }}</label>
                     </div>
                 </div>
-                <div class="w-full lg:w-4/12 px-4 flex-row">
-                    <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">صادره از</label>
-                        <label
-                            class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
-                                datapersonal.Sodor
-                            }}</label>
-                    </div>
-                </div>
-                <div class="w-full lg:w-4/12 px-4 flex-row">
-                    <div class="relative w-full mb-3">
-                        <label class="block uppercase  text-xs font-bold mb-2" htmlfor="grid-password">ملیت</label>
-                        <label
-                            class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{
-                            }}</label>
-                    </div>
-                </div>
+<!--                <div class="w-full lg:w-4/12 px-4 flex-row">-->
+<!--                    <div class="relative w-full mb-3">-->
+<!--                        <label class="block uppercase  text-xs font-bold mb-2">صادره از</label>-->
+<!--                        <label-->
+<!--                            class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{-->
+<!--                                datapersonal.Sodor-->
+<!--                            }}</label>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="w-full lg:w-4/12 px-4 flex-row">-->
+<!--                    <div class="relative w-full mb-3">-->
+<!--                        <label class="block uppercase  text-xs font-bold mb-2">ملیت</label>-->
+<!--                        <label-->
+<!--                            class="cursor-not-allowed border border-colorborder px-3 py-3   bg-c-gray rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">{{-->
+<!--                            }}</label>-->
+<!--                    </div>-->
+<!--                </div>-->
             </div>
         </form>
     </div>
@@ -129,7 +131,8 @@ export default {
     props: ['datapersonal'],
     data() {
         return {
-            imageSrc: [],
+            imageSrc: '',
+            datapersonals:[],
         };
     },
     methods: {
@@ -150,18 +153,15 @@ export default {
                 });
         },
 
-        getPersonalImage() {
-            axios.get(`/api/getprofileimage/this/${this.nationalcode}/`)
-                .then(response => {
-                    this.imageSrc = response.data;
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        }
     },
-    created() {
-        this.getPersonalImage();
+    mounted() {
+        axios.get(`/api/getprofileimage/this/0371714941`)
+            .then(response => {
+                this.imageSrc = response.data.imageSrc;
+            })
+            .catch(error => {
+                console.log(error)
+            });
     }
 }
 </script>
