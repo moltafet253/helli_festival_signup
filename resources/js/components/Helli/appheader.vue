011376fa3e05444c0cafdcf4bee76790b179004c
exploiter0253 <exploiter0253@gmail.com>
2023-05-20 13:55:19 +0330

    For Pulling With SSH => Adding Help File

--- resources/js/components/Helli/appheader.vue
+++ resources/js/components/Helli/appheader.vue
@@ -3,7 +3,7 @@
         <nav class="bg-navbar-base mb2 text-white px-10 border-b-8 border-c-orange">
             <div class="container flex items-center justify-between px-4 py-2">
                 <div class="mb-2">
-                    <a href="https://pazhuhesh.ismc.ir" class="mr-4 text-white">راهنمای کار با سامانه</a>
+                    <a href="./pub_files/راهنمای ثبت نام در جشنواره.pdf" class="mr-4 text-white">راهنمای کار با سامانه</a>
 <!--                    <a href="https://pazhuhesh.ismc.ir" class="mr-16 text-white">ورود به سایت خبری معاونت پژوهش</a>-->
                 </div>
                 <div>
