f27b762e429c3d4863ab6eae2d829c30d8fac255
exploiter0253 <exploiter0253@gmail.com>
2023-05-24 10:06:56 +0330

    For Pulling With SSH => Fix Deleting Post

--- app/Http/Controllers/api/DeletePost.php
+++ app/Http/Controllers/api/DeletePost.php
@@ -8,6 +8,7 @@ use App\Models\Helli\Post;
 use App\Models\User;
 use App\Models\UserActivityLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Jenssegers\Agent\Agent;
 
 class DeletePost extends Controller
@@ -16,7 +17,8 @@ class DeletePost extends Controller
     {
         if ($id) {
             $post = Post::find($id);
-            $mainUser = User::find($post['user_id'])->value('national_code');
+            $mainUser = DB::table('users')->select('national_code')->where('id',$post['user_id'])->get();
+            $mainUser = $mainUser[0]->national_code;
             HelliUserMaxUploadPost::where('national_code', '=', $mainUser)->increment('numbers', 1);
             $post->assigned_post_id=null;
             $post->sent=2;
