e25109f76abfde2e476fba2e59c40fb4e0f17816
exploiter0253 <exploiter0253@gmail.com>
2023-05-29 11:59:04 +0330

    For Pulling With SSH => Fix Previous Migration

--- database/migrations/2023_05_29_115413_modify_educational_infos_table_change_shparvandetahsili.php
+++ database/migrations/2023_05_29_115413_modify_educational_infos_table_change_shparvandetahsili.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('educational_infos', function (Blueprint $table) {
-            $table->string('shparvandetahsili')->change();
+            $table->string('shparvandetahsili')->nullable()->change();
         });
     }
 
@@ -21,7 +21,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('educational_infos', function (Blueprint $table) {
-            $table->integer('shparvandetahsili')->change();
+            $table->integer('shparvandetahsili')->nullable()->change();
         });
     }
 };
