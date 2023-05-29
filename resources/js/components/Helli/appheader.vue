8771f9c545ea75505e6b76949a4f9b4a35f71ee9
exploiter0253 <exploiter0253@gmail.com>
2023-05-29 10:24:55 +0330

    For Pulling With SSH => Add Signup Conditions File And Menu In Header And Change Help File

--- resources/js/components/Helli/appheader.vue
+++ resources/js/components/Helli/appheader.vue
@@ -3,8 +3,9 @@
         <nav class="bg-navbar-base mb2 text-white px-10 border-b-8 border-c-orange">
             <div class="container flex items-center justify-between px-4 py-2">
                 <div class="mb-2">
-                    <a href="./pub_files/راهنمای ثبت نام در جشنواره.pdf" class="mr-4 text-white">راهنمای کار با سامانه</a>
-<!--                    <a href="https://pazhuhesh.ismc.ir" class="mr-16 text-white">ورود به سایت خبری معاونت پژوهش</a>-->
+                    <a href="./pub_files/help.pdf" class="mr-4 text-white">راهنمای کار با سامانه</a>
+                    &nbsp;
+                    <a href="./pub_files/conditions.pdf" class="mr-4 text-white">شرایط شرکت در جشنواره</a>
                 </div>
                 <div>
                     <img src="build/assets/icons/Logo.svg" alt="لوگو" class="h-14">
