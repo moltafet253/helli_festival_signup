92b32dbc1740428c439751e2cbc53aacfc5b4dc6
exploiter0253 <exploiter0253@gmail.com>
2023-05-29 12:05:04 +0330

    For Pulling With SSH => Revert $tahsilatghhozavi Condition For LastSendPosts.php

--- app/Http/Controllers/api/LastSendPosts.php
+++ app/Http/Controllers/api/LastSendPosts.php
@@ -117,33 +117,34 @@ class LastSendPosts extends Controller
                 }
             }
 
-            $tahsilatghhozavi = $educationalInfo[0]['tahsilatghhozavi'];
-            switch ($tahsilatghhozavi) {
-                case 'لیسانس':
-                    switch ($rateLevel) {
-                        case 1:
-                        case 2:
-                        case 3:
-                            $rateLevel++;
-                            break;
-                    }
-                    break;
-                case 'فوق لیسانس':
-                    switch ($rateLevel) {
-                        case 1:
-                        case 2:
-                            $rateLevel += 2;
-                            break;
-                        case 3:
-                            $rateLevel += 1;
-                            break;
-                    }
-                    break;
-                case 'دکتری':
-                    $rateLevel = 4;
-                    break;
+            if ($post['research_format'] == 'مقاله' or $post['research_format'] == 'کتاب') {
+                $tahsilatghhozavi = $educationalInfo[0]['tahsilatghhozavi'];
+                switch ($tahsilatghhozavi) {
+                    case 'لیسانس':
+                        switch ($rateLevel) {
+                            case 1:
+                            case 2:
+                            case 3:
+                                $rateLevel++;
+                                break;
+                        }
+                        break;
+                    case 'فوق لیسانس':
+                        switch ($rateLevel) {
+                            case 1:
+                            case 2:
+                                $rateLevel += 2;
+                                break;
+                            case 3:
+                                $rateLevel += 1;
+                                break;
+                        }
+                        break;
+                    case 'دکتری':
+                        $rateLevel = 4;
+                        break;
+                }
             }
-
 
             if ($post['special_section'] != null and $post['special_section'] != 'null' and $post['special_section'] != '') {
                 $hasSpecial = 'هست';
