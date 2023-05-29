e474c3c48d6fb8b2f486259c42391680c07e28ec
exploiter0253 <exploiter0253@gmail.com>
2023-05-29 10:28:16 +0330

    For Pulling With SSH => Add Target=_blank To Header Links And Add Pazhuhesh Website Link To Logo

--- resources/js/components/Helli/appheader.vue
+++ resources/js/components/Helli/appheader.vue
@@ -3,12 +3,14 @@
         <nav class="bg-navbar-base mb2 text-white px-10 border-b-8 border-c-orange">
             <div class="container flex items-center justify-between px-4 py-2">
                 <div class="mb-2">
-                    <a href="./pub_files/help.pdf" class="mr-4 text-white">راهنمای کار با سامانه</a>
+                    <a target="_blank" href="./pub_files/help.pdf" class="mr-4 text-white">راهنمای کار با سامانه</a>
                     &nbsp;
-                    <a href="./pub_files/conditions.pdf" class="mr-4 text-white">شرایط شرکت در جشنواره</a>
+                    <a target="_blank" href="./pub_files/conditions.pdf" class="mr-4 text-white">شرایط شرکت در جشنواره</a>
                 </div>
                 <div>
-                    <img src="build/assets/icons/Logo.svg" alt="لوگو" class="h-14">
+                    <a href="https://pazhuhesh.ismc.ir" target="_blank">
+                        <img src="build/assets/icons/Logo.svg" alt="لوگو" class="h-14">
+                    </a>
                 </div>
             </div>
         </nav>
